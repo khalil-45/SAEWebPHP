d73333a0085b0144f4b6762be6ab7d494b12638e
Yannis Barache <112858061+Yannis-barache@users.noreply.github.com>
2024-02-18 23:51:01 +0100

    Bon nombre dans l'affichage des chansons

--- templates/renders/chansons.php
+++ templates/renders/chansons.php
@@ -2,43 +2,43 @@
 
 <section class="titres">
     <ul>
-        <?php foreach ($chansons as $chanson) : ?>
-            <li>
-                <p>
-                    <?php echo $chanson->getChansonId() ?>
-                </p>
-                <img src="../../static/images/bouton-jouer-petit.png" alt="bouton play">
-                <?php echo $chanson->getTitre() ;
-                if (isset($_SESSION['user'])) : ?>
-                <button class="add-to-playlist-button" onclick="showPlaylistsPopup(<?php echo $chanson->getChansonId() ?>)">Ajouter à la
-                    playlist</button>
-                <?php else : ?>
-                <button class="add-to-playlist-button" onclick="openFormLogIn()">Ajouter à la playlist</button>
-                <?php endif; ?>
+        <?php foreach ($chansons as $index => $chanson) : ?>
+    <li>
+        <p>
+            <?php echo $index + 1; // Adding 1 because array indices start at 0 ?>
+        </p>
+        <img src="../../static/images/bouton-jouer-petit.png" alt="bouton play">
+        <?php echo $chanson->getTitre();
+        if (isset($_SESSION['user'])) : ?>
+        <button class="add-to-playlist-button" onclick="showPlaylistsPopup(<?php echo $index + 1; ?>)">Ajouter à la
+            playlist</button>
+        <?php else : ?>
+        <button class="add-to-playlist-button" onclick="openFormLogIn()">Ajouter à la playlist</button>
+        <?php endif; ?>
 
-                <!-- Add a form to select a playlist -->
-                <div id="popup-playlists<?php echo $chanson->getChansonId() ?>" class="popup-playlists" style="display: none;">
-                    <div class="popup-content">
-                        <span class="close-popup-playlists" onclick="closePlaylistsPopup(<?php echo $chanson->getChansonId() ?>)">&times;</span>
-                        <h3>Ajouter à la playlist du titre :
-                            <?php echo $chanson->getTitre() ?>
-                        </h3>
+        <!-- Add a form to select a playlist -->
+        <div id="popup-playlists<?php echo $index + 1; ?>" class="popup-playlists" style="display: none;">
+            <div class="popup-content">
+                <span class="close-popup-playlists" onclick="closePlaylistsPopup(<?php echo $index + 1; ?>)">&times;</span>
+                <h3>Ajouter à la playlist du titre :
+                    <?php echo $chanson->getTitre() ?>
+                </h3>
 
-                        <form action="../../app/Model/ajout_chanson_playlist.php" method="post">
-                            <input type="hidden" name="chanson_id" value="<?php echo $chanson->getChansonId() ?>">
+                <form action="../../app/Model/ajout_chanson_playlist.php" method="post">
+                    <input type="hidden" name="chanson_id" value="<?php echo $index + 1; ?>">
 
-                            <label for="playlist">Sélectionner une playlist :</label>
-                            <?php foreach ($playlists as $p) : ?>
-                                <button type="submit" name="playlist" value="<?php echo $p->getPlaylistId() ?>">
-                                    <?php echo $p->getTitre() ?>
-                                </button>
-                            <?php endforeach; ?>
-                        </form>
+                    <label for="playlist">Sélectionner une playlist :</label>
+                    <?php foreach ($playlists as $p) : ?>
+                        <button type="submit" name="playlist" value="<?php echo $p->getPlaylistId() ?>">
+                            <?php echo $p->getTitre() ?>
+                        </button>
+                    <?php endforeach; ?>
+                </form>
 
-                    </div>
-                </div>
-            </li>
-        <?php endforeach; ?>
+            </div>
+        </div>
+    </li>
+<?php endforeach; ?>
     </ul>
 </section>
 <a class="retourbtn" href="<?php echo $_SERVER['HTTP_REFERER']?>"><p>Retour</p></a>

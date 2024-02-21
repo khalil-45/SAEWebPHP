172d58d71859b6c105e26e775c28bc385a824548
Yannis Barache <112858061+Yannis-barache@users.noreply.github.com>
2024-02-21 11:59:03 +0100

    Correction erreur_id

--- templates/renders/chansons.php
+++ templates/renders/chansons.php
@@ -10,22 +10,22 @@
         <img src="../../static/images/bouton-jouer-petit.png" alt="bouton play">
         <?php echo $chanson->getTitre();
         if (isset($_SESSION['user'])) : ?>
-        <button class="add-to-playlist-button" onclick="showPlaylistsPopup(<?php echo $index + 1; ?>)">Ajouter à la
+        <button class="add-to-playlist-button" onclick="showPlaylistsPopup(<?php echo $chanson->getChansonId() ; ?>)">Ajouter à la
             playlist</button>
         <?php else : ?>
         <button class="add-to-playlist-button" onclick="openFormLogIn()">Ajouter à la playlist</button>
         <?php endif; ?>
 
         <!-- Add a form to select a playlist -->
-        <div id="popup-playlists<?php echo $index + 1; ?>" class="popup-playlists" style="display: none;">
+        <div id="popup-playlists<?php echo $chanson->getChansonId() ; ?>" class="popup-playlists" style="display: none;">
             <div class="popup-content">
-                <span class="close-popup-playlists" onclick="closePlaylistsPopup(<?php echo $index + 1; ?>)">&times;</span>
+                <span class="close-popup-playlists" onclick="closePlaylistsPopup(<?php echo $chanson->getChansonId(); ?>)">&times;</span>
                 <h3>Ajouter à la playlist du titre :
                     <?php echo $chanson->getTitre() ?>
                 </h3>
 
                 <form action="../../app/Model/ajout_chanson_playlist.php" method="post">
-                    <input type="hidden" name="chanson_id" value="<?php echo $index + 1; ?>">
+                    <input type="hidden" name="chanson_id" value="<?php echo $chanson->getChansonId() ?>">
 
                     <label for="playlist">Sélectionner une playlist :</label>
                     <?php foreach ($playlists as $p) : ?>
